f30c5cd8898d30828a36152e57d09351d2c2427f
adii.dg <adii.dg.2021@gmail.com>
2026-01-05 10:55:52 +0530

    neww

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiKey: 'AIzaSyB0IQBE1W2AwnQddabV4avGqgL4xojJKsw',  // Use a different API key for production
+  firebaseConfig : {
+    apiKey: "AIzaSyD914Al0IS7L9tBN6u-g6vEauvxcEQhlfA",
+    authDomain: "hostelight-adiidg.firebaseapp.com",
+    projectId: "hostelight-adiidg",
+    storageBucket: "hostelight-adiidg.firebasestorage.app",
+    messagingSenderId: "21896924218",
+    appId: "1:21896924218:web:d341bf7a8584d61d3bf903"
+}
+};
